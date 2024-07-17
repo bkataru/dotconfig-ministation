4c94a00b134acc339c6b0079ab62b0ad34144fab
bkataru <baalateja.k@gmail.com>
2024-07-17 16:06:25 +0530

    fixed clippy not firing via rust-tools.nvim in rust code

--- nvim/lua/configs/rust-tools.lua
+++ nvim/lua/configs/rust-tools.lua
@@ -9,7 +9,36 @@ local options = {
     on_attach = on_attach,
     on_init = on_init,
     capabilities = capabilities,
-  }
+    settings = {
+      ["rust-analyzer"] = {
+        cargo = {
+          all_features = true,
+          loadOutDirsFromCheck = true,
+          runBuildScripts = true,
+        },
+        checkOnSave = {
+          allFeatures = true,
+          command = "clippy",
+          extraArgs = {
+            "--",
+            "--no-deps",
+            "-Dclippy::correctness",
+            "-Dclippy::complexity",
+            "-Wclippy:perf",
+            "-Wclippy::pedantic",
+          },
+        },
+        procMacro = {
+          enable = true,
+          ignored = {
+            ["async-trait"] = { "async_trait "},
+            ["napi-derive"] = { "napi" },
+            ["async-recursion"] = { "async_recursion" },
+          },
+        },
+      },
+    },
+  },
 }
 
 return options
